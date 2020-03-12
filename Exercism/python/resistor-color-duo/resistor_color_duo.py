508c265927dba022ffdb408204521a3b53c88544
Dusknior <43151182+Dusknior@users.noreply.github.com>
2020-03-11 22:16:11 -0500

    Add files via upload

---
+++ Exercism/python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,8 @@
+colores = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4,
+           "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+
+
+def value(colors):
+    color1 = colors[0]
+    color2 = colors[1]
+    return int((str(colores[color1]) + str(colores[color2])))
